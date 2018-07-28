e85b7d064c372fa4307a9efc9843bc1737afe5c8
Pablo Ledesma <pabloledes83@gmail.com>
2018-07-28 06:03:49 -0500

    Test-driving-threads

--- tests/Feature/ThreadsTest.php
+++ tests/Feature/ThreadsTest.php
@@ -3,15 +3,29 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class ThreadsTest extends TestCase
 {
+    use DatabaseMigrations;
+
     /** @test */
     public function a_user_can_browse_threads()
     {
+        $thread = factory('App\Thread')->create();
+        
         $response = $this->get('/threads');
+        
+        $response->assertSee($thread->title);
+    }
+    
+    /** @test */
+    function A_user_can_read_a_single_thread()
+    {
+        $thread = factory('App\Thread')->create();
 
-        $response->assertStatus(200);
+        $response = $this->get('/threads/' . $thread->id);
+        
+        $response->assertSee($thread->title);
     }
 }
