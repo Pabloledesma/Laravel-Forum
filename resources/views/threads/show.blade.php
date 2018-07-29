952ae2689d10335030738eee1c4542a7fd9722d7
Pablo Ledesma <pabloledes83@gmail.com>
2018-07-29 08:32:05 -0500

    the reply forum

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -19,5 +19,28 @@
             </div>
         </div>
     </div>
+    @if(auth()->check())
+    <div class="row">
+        <div class="col-md-12">
+            <form method="POST" action="{{ $thread->path() . '/replies' }}">
+
+                {{ csrf_field() }}
+
+                <div class="form-group">
+                    <textarea 
+                        name="body" 
+                        id="body" 
+                        class="form-control"
+                        rows="5" 
+                        placeholder="Have something to say?"></textarea>
+                </div>
+
+                <button type="submit" class="btn btn-default">Post</button>
+            </form>
+        </div>
+    </div>
+    @else
+        <p>Please <a href="{{ route('login') }}">sign in</a> to participate in this discussion.</p>
+    @endif
 </div>
 @endsection
